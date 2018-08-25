6a69f40f23ecf59283b05415221520b3cc5b424d
sintanbabo <sintanbabo@sudal-mac-mini.local>
2018-08-25 17:17:44 +0900

    1

---
+++ src/main/java/com/sintanbabo/springmvc4/config/RootConfig.java
@@ -0,0 +1,9 @@
+package com.sintanbabo.springmvc4.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+   //Service and Repository beans configuration
+   //....
+}
